2d40df6a38c68781760497ece68bc7a8f35b1cbe
sevinc145 <sevinc.abdullayeva2008@gmail.com>
2025-05-19 14:06:14 +0400

    Update Card.jsx

--- src/Component/Main/Card.jsx
+++ src/Component/Main/Card.jsx
@@ -10,7 +10,7 @@ function Card({name,capital,flags, cca3}) {
             <div className="p-6 space-y-2">
                 <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">{name.common}</h3>
                 <span className="text-xs dark:text-gray-400">{capital !== undefined && capital.map(item => item)}</span>
-                <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
+                <p>Zeynallll</p>
             </div>
         </Link>
     )
